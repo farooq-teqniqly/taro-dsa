c706484929e82b79657db65788ab0cdbdc496808
farooq-teqniqly <farooq@teqniqly.com>
2024-12-05 07:53:50 -0800

    chore: renaming test file

--- TaroDSA.Tests/CoreProblemsTests.cs
+++ TaroDSA.Tests/CoreProblemsTests.cs
@@ -3,10 +3,10 @@ using TaroDSA.Lib;
 
 namespace TaroDSA.Tests;
 
-public class CoreProblemTests
+public class CoreProblemsTests
 {
     [Theory]
-    [InlineData(new [] {4, 7, 2, 8, 10, 9}, 10)]
+    [InlineData(new[] { 4, 7, 2, 8, 10, 9 }, 10)]
     [InlineData(new[] { -4, -7, -2, -8, -10, -9 }, -2)]
     [InlineData(new int[0], int.MinValue)]
     public void Can_Find_Max(int[] numbers, int expectedResult)
